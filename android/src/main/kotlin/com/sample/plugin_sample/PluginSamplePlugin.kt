48abb452c1344efe72d8726efcf87766f163178c
Jose S <santiagojoed@gmail.com>
2022-07-11 15:41:32 +0800

    Code for non-coroutine implementation

--- android/src/main/kotlin/com/sample/plugin_sample/PluginSamplePlugin.kt
+++ android/src/main/kotlin/com/sample/plugin_sample/PluginSamplePlugin.kt
@@ -1,15 +1,23 @@
 package com.sample.plugin_sample
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.annotation.NonNull
+import io.flutter.embedding.engine.FlutterEngine
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.Executors
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 /** PluginSamplePlugin */
@@ -19,54 +27,31 @@ class PluginSamplePlugin : FlutterPlugin, MethodCallHandler {
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
+    var engine: FlutterEngine? = null
 
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "plugin_sample")
         channel.setMethodCallHandler(this)
+
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
         if (call.method == "getPlatformVersion") {
-           result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
 
             val validation = object : Validation {
                 override fun validate(value: String): Boolean {
-
-                    Log.d("Validation", "Validating $value from Android")
-
-                    val validationResult =
-                        runBlocking {
-                        return@runBlocking suspendCoroutine<Boolean> {
-                            channel.invokeMethod("validate", value, object : MethodChannel.Result {
-                                override fun success(p0: Any?) {
-                                    Log.d("Validation Result", "$p0")
-                                    it.resume(p0 as Boolean)
-
-                                }
-
-                                override fun error(p0: String, p1: String?, p2: Any?) {
-                                    Log.d("Validation Result", "Error $p0")
-
-                                }
-
-                                override fun notImplemented() {
-                                    Log.d("Validation Result", "Not implemented")
-
-                                }
-
-                            })
-                        }
-                    }
-
-                    Log.d("Validation", "Validation completed, result = $validationResult")
-
-
-                    return validationResult
+                    return invokeMethod("validate", value) as Boolean
                 }
 
             }
 
-            validation.validate("Sample Input")
+            Executors.newSingleThreadExecutor().execute {
+                val result = validation.validate("Sample Input")
+                Log.d("Validation", "Result:$result")
+
+            }
+
 
 
         } else {
@@ -74,9 +59,47 @@ class PluginSamplePlugin : FlutterPlugin, MethodCallHandler {
         }
     }
 
+    @Synchronized
+    fun invokeMethod(method: String, arguments: Any?): Any? {
+        var callResult = CallResult()
+        Handler(Looper.getMainLooper()).post {
+            channel.invokeMethod(method, arguments, object : MethodChannel.Result {
+                override fun success(result: Any?) {
+                    callResult.value = result
+                    callResult.completed = true
+                }
+
+                override fun error(p0: String, p1: String?, p2: Any?) {
+                    callResult.value = null
+                    callResult.completed = true
+                }
+
+                override fun notImplemented() {
+                    callResult.value = null
+                    callResult.completed = true
+                }
+            })
+        }
+        try {
+            while (!callResult.completed) {
+                Log.d("Validation","Waiting for result...")
+                // block
+            }
+        } catch (e: InterruptedException) {
+            return null
+        }
+        return callResult.value
+    }
+
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
     }
+
+
+    private class CallResult(
+        @Volatile var completed: Boolean = false,
+        @Volatile var value: Any? = null
+    )
 }
 
 
